181beb9b3c032ebd1a380905e7a470303c119aa3
Michael <michael.melendezm@gmail.com>
2017-05-03 16:43:40 -0600

    Sin combos para la ubicacion

--- app/src/main/java/com/mjb/projectexperts/GridViewImageAdapter.java
+++ app/src/main/java/com/mjb/projectexperts/GridViewImageAdapter.java
@@ -23,7 +23,7 @@ public class GridViewImageAdapter extends BaseAdapter {
     public GridViewImageAdapter(Context c){
         mContext = c;
         //se cargan las miniaturas
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < 9; i++){
             listPhoto.add("http://rentacarcostarica.com/portal/wp-content/uploads/2016/09/Prusia-Park-is-part-of-the-Iraz%C3%BA-National-Park.jpg");
         }
     }
